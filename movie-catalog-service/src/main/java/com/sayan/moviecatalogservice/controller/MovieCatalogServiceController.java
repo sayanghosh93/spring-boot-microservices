35ed3d27321d85a90e682eea94bad9a39ee93554
Sayan Ghosh <sayan.ghosh@intelligentappsolutions.com>
2021-02-18 14:47:44 +0530

    Implemented Service Discovery with Eureka Server and Implemented Eureka Client ijn the microservices

--- movie-catalog-service/src/main/java/com/sayan/moviecatalogservice/controller/MovieCatalogServiceController.java
+++ movie-catalog-service/src/main/java/com/sayan/moviecatalogservice/controller/MovieCatalogServiceController.java
@@ -9,6 +9,8 @@ import com.sayan.moviecatalogservice.entity.MovieEntity;
 import com.sayan.moviecatalogservice.entity.RatingDataEntity;
 import com.sayan.moviecatalogservice.entity.UserRatingModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import com.sayan.moviecatalogservice.entity.CatalogEntity;
@@ -19,23 +21,32 @@ import org.springframework.web.reactive.function.client.WebClient;
 @RequestMapping("/catalog")
 public class MovieCatalogServiceController {
 
-	@Autowired
-	private RestTemplate restTemplate;
+    @Autowired
+    private RestTemplate restTemplate;
 
-	@Autowired
-	private WebClient.Builder webClientBuilder;
+    @Autowired
+    private WebClient.Builder webClientBuilder;
 
-	@GetMapping("/{userId}")
-	public List<CatalogEntity> getCatalog(@PathVariable("userId") String userId) {
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
-		UserRatingModel ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" + userId, UserRatingModel.class);
+    @GetMapping("/{userId}")
+    public List<CatalogEntity> getCatalog(@PathVariable("userId") String userId) {
+        List<ServiceInstance> serviceInstanceList = discoveryClient.getInstances("ratings-data-service");
+        for (ServiceInstance instance :
+                serviceInstanceList) {
+            //Load Balancing ??
+            System.out.println("Available Instances: " + instance.getHost() + ":" + instance.getPort());
+        }
 
-		return ratings.getList().stream().map(rating ->{
-					MovieEntity movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), MovieEntity.class);
-					return new CatalogEntity(movie.getName(), "Test Description", rating.getRating());
-		})
-		.collect(Collectors.toList());
-	}
+        UserRatingModel ratings = restTemplate.getForObject("http://rating-data-service/ratingsdata/users/" + userId, UserRatingModel.class);
+
+        return ratings.getList().stream().map(rating -> {
+            MovieEntity movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), MovieEntity.class);
+            return new CatalogEntity(movie.getName(), "Test Description", rating.getRating());
+        })
+                .collect(Collectors.toList());
+    }
 }
 
 /*MovieEntity movie = webClientBuilder.build()
